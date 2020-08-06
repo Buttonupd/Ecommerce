bf3eef819336857d1db60a238f7c8f5ef7bb9198
Buttonupd <Dankariuki0101@gmail.com>
2020-08-06 13:42:44 +0300

    Created a base, local, and production settings file

---
+++ src/accounts/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, FormView, DeleteView, View, UpdateView
+from django.views.generic.edit import FormMixin
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.utils.http import is_safe_url
+from django.utils.safestring import mark_safe
+
+# from ecommer
